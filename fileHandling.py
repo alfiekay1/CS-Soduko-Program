cbec0033e03c0c72d0cc8f6b3986ccbb2cce56cf
Jack Huskinson <9900665504@exe-coll.ac.uk>
2026-09-24 15:09:18 +0000

    Finish file fileHandling

--- fileHandling.py
+++ fileHandling.py
@@ -1,2 +1,23 @@
 def load_board (file_name):
-    file = open(file_name, "r")
+    found = False
+    while not found:
+        try:
+            file = open(file_name, "r")
+            content = file.read()
+            found = True
+        except:
+            file_name = input("File not found, input valid file: ")
+    return content
+    file.close()
+def cast_board (board):
+    numbers = board.split (" ")
+    print (numbers)
+    array_board = []
+ 
+    for i in range (9):
+        row = []
+        for j in range (9):
+            row.append (numbers[9*i + j])
+ 
+        array_board.append (row)
+    return array_board
